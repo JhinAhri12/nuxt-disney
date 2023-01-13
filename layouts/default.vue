a2848a490b2c8e2c1dee14689be13cb7fabecf9f
JhinAhri12 <cyprien.rezki@hotmail.fr>
2023-01-13 17:25:44 +0100

    add header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,15 @@
 <template>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <div id="app">
-        Disney
+        <header class="">
+        <nav class="container mx-auto p-4 flex justify-between">
+            <NuxtLink to="/" class="font-bold">Nuxt Disney</NuxtLink>
+            <ul class="flex gap-4">
+                <li><NuxtLink to="/">Home</NuxtLink></li>
+                <li><NuxtLink to="/disney">Disney</NuxtLink></li>
+            </ul>
+        </nav>
+        </header>
        <!-- layout -->
         <div class="container">
             <slot />  
