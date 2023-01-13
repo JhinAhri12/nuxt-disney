b2a1582223838da3aef445d4c5213a29cfb803ff
JhinAhri12 <cyprien.rezki@hotmail.fr>
2023-01-13 18:06:32 +0100

    add searchbar

---
+++ components/DisneySearch.vue
@@ -0,0 +1,22 @@
+<template>
+    
+    <form @submit.prevent="onSubmit">
+        <label for="name">Name:</label>
+        <input id="name" v-model="name">
+        <input class="button" type="submit" value="Submit"> 
+    </form>
+    
+</template>
+
+<script setup>
+    let name = '';
+    async function onSubmit (){
+        if (name === '' ) {
+          alert('Search is incomplete. Please fill out every field.')
+          return
+        }else{
+            const { data: disneyCharacter } = await useFetch(`https://api.disneyapi.dev/character?name=${name}`)
+            console.log(disneyCharacter)
+        }
+    }
+</script>
--- pages/disney/index.vue
+++ pages/disney/index.vue
@@ -1,8 +1,10 @@
 <template>
+    <DisneySearch />
     <div class="col-3" v-for="d in disneys">
         <DisneyCard class="card mb-3" :disney="d" />
     </div>
 </template>
 <script setup>
-    const { data: disneys } = await useFetch('https://api.disneyapi.dev/characters')
+    const { data: disneys } = await useFetch('https://api.disneyapi.dev/characters' )
+   
 </script>
