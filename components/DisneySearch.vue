840d9f5540b04623f6151b6d30404f377b01e775
JhinAhri12 <cyprien.rezki@hotmail.fr>
2023-01-13 18:23:14 +0100

    add result

--- components/DisneySearch.vue
+++ components/DisneySearch.vue
@@ -5,18 +5,27 @@
         <input id="name" v-model="name">
         <input class="button" type="submit" value="Submit"> 
     </form>
-    
+
+    <div v-for="r in result">
+        <div v-for="character in r">
+            <NuxtLink :to="`/disney/${character._id}`">
+                <h3 class="">{{ character.name }}</h3>
+            </NuxtLink>
+        </div>
+    </div>
+    <hr>
 </template>
 
 <script setup>
     let name = '';
+    let result = '';
     async function onSubmit (){
         if (name === '' ) {
           alert('Search is incomplete. Please fill out every field.')
           return
         }else{
             const { data: disneyCharacter } = await useFetch(`https://api.disneyapi.dev/character?name=${name}`)
-            console.log(disneyCharacter)
+            result = disneyCharacter
         }
     }
 </script>
