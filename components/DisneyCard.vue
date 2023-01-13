9bfc3998d668441c1d68203276a08e5a87106a6d
JhinAhri12 <cyprien.rezki@hotmail.fr>
2023-01-13 17:22:37 +0100

    resume project

---
+++ components/DisneyCard.vue
@@ -0,0 +1,13 @@
+<template>
+    
+    <NuxtLink :to="`/disney/${disney.id}`">
+
+        <h3 class="card-title text-center">{{ disney.title }}</h3>
+        <img class="card-img-bottom product-img mx-auto mb-2 img-thumbnail" :src="disney.image" alt="">
+    </NuxtLink>
+</template>
+
+<script setup>
+    const { disney } = defineProps(['disney'])
+    
+</script>
---
+++ pages/disney/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="col-3" v-for="d in disneys">
+        <DisneyCard class="card mb-3" :disney="d" />
+    </div>
+</template>
+<script setup>
+    const { data: disneys } = await useFetch('https://api.disneyapi.dev/characters')
+</script>
