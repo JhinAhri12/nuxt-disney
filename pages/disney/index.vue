b34c1d6e7b3ac32c0fa0a0723925997c842b3178
JhinAhri12 <cyprien.rezki@hotmail.fr>
2023-01-19 19:42:23 +0100

    add error if api is down

--- pages/disney/index.vue
+++ pages/disney/index.vue
@@ -3,8 +3,21 @@
     <div class="col-3" v-for="d in disneys">
         <DisneyCard class="card mb-3" :disney="d" />
     </div>
+    <div v-if="page === 1"> 
+        Page 1 sur {{ disneys.totalPages }}
+        <NuxtLink :to="`/disney?page=2`">
+            Page suivante
+        </NuxtLink>
+    </div>
+    
 </template>
 <script setup>
+    
+    let page = 1
+    let nextPage = page + 1
     const { data: disneys } = await useFetch('https://api.disneyapi.dev/characters' )
+    
+    if (!disneys.value) {  throw createError({ statusCode: 404, statusMessage: 'Error in the api' })}
+    
    
 </script>
