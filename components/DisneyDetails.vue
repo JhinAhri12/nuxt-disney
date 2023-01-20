35f46ecf62430a0f19a306c511a343e5a9a9dd43
JhinAhri12 <cyprien.rezki@hotmail.fr>
2023-01-20 10:03:34 +0100

    add some disneydetails and fetch details

---
+++ components/DisneyDetails.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="card" style="width: 18rem;">
+        
+        <img class="card-img-top" :src="disney.imageUrl" alt="">
+        <div class="card-body text-center">
+            <h3>{{ disney.name }}</h3>
+            <button class="btn btn-info" style="color:white">Ajouter à mes favoris</button>
+        </div>
+    </div>
+  </template>
+
+<script setup>
+  const { disney } = defineProps(['disney'])
+</script>
--- pages/disney/[id].vue
+++ pages/disney/[id].vue
@@ -1,3 +1,13 @@
 <template>
-    
+    <DisneyDetails :disney="disney" />
 </template>
+<script setup>
+  const { id } = useRoute().params
+  const uri = 'https://api.disneyapi.dev/characters/' + id
+
+  const { data: disney } = await useFetch(uri, { key: id })
+  if (!disney.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Character not found' })
+  }
+
+</script>
