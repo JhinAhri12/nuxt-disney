f637b53dc0b95050b5f030851ad2898228be2e75
JhinAhri12 <cyprien.rezki@hotmail.fr>
2023-02-01 16:45:50 +0100

    add prisma ans schema

--- components/DisneyDetails.vue
+++ components/DisneyDetails.vue
@@ -4,11 +4,14 @@
         <img class="card-img-top" :src="disney.imageUrl" alt="">
         <div class="card-body text-center">
             <h3>{{ disney.name }}</h3>
-            <button class="btn btn-info" style="color:white">Ajouter à mes favoris</button>
+            <button class="btn btn-info" style="color:white" @click="addToFavorite">Ajouter à mes favoris</button>
         </div>
     </div>
   </template>
 
 <script setup>
   const { disney } = defineProps(['disney'])
+
+  const addToFavorite = await useFetch(() => `https://api.disneyapi.dev/character?name=${page.value}`)
+
 </script>
